05fc6650953e5096e158a370202619aeccd467ff
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-07-14 17:24:13 +0530

    Add files via upload

---
+++ ReactJS-Assignment-1-LetsUpgrad/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./style.css"
+import Button from "./Button"
+
+const App=()=>{
+    return(
+        <div>
+            <h1 style={{textAlign : "center"}}> Hello World</h1>
+            <h2> Namaste React.js </h2>
+            <Button buttonText="React.js"/>
+            <Button buttonText="Node.js"/>
+            <Button buttonText="Express.js"/>
+        </div>
+    )
+}
+
+export default App
